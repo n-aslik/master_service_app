30d6a3d874893c654985c0e51260511d120c567f
asliddin <aslnabiev2002@gmail.com>
2025-09-02 10:41:12 +0500

    Update socket

--- package/controller/chat.py
+++ package/controller/chat.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, APIRouter
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect, APIRouter,Depends
 from fastapi.responses import HTMLResponse
 from typing import Dict
 from package.repository import client_module
+from package.service import jwt_hand as JWTHandler
 
 class ConnectionManager:
     def __init__(self):
@@ -37,7 +38,8 @@ manager = ConnectionManager()
 
 
 @router.websocket("/{room_id}/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, room_id:int, user_id: str, firstname: str):
+async def websocket_endpoint(websocket: WebSocket, room_id:int, user_id: str, firstname: str, payload: dict = Depends(JWTHandler.access_token)):
+    user_id = payload["user_id"]
     await manager.connect(websocket, room_id, user_id)
     await manager.broadcast(f"{firstname} (ID: {user_id}) присоединился к чату", room_id, user_id)
     try:
